2ac2a3ef58d95eab70e06509051983b2df0e283e
thiago morgado <thiago_morgado@hotmail.com>
2024-04-30 14:10:48 -0300

    estrutura de pasta e arquivo de navigation

--- src/Routes/MainStack.tsx
+++ src/Routes/MainStack.tsx
@@ -3,15 +3,22 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 // Screens
 import  Home  from '../screens/Home';
+import ForgottenPassword from '../screens/ForgotenPassword';
+import SignIn from '../screens/SignIn';
+import SignUp from '../screens/SignUp';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function MainStack() {
   return(
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName='criarConta'
+    >
       <Stack.Screen name="Home" component={Home} />
-      
+      <Stack.Screen name="ForgottenPassword" component={ForgottenPassword} />
+      <Stack.Screen name="Login" component={SignIn} />
+      <Stack.Screen name="criarConta" component={SignUp} />
     </Stack.Navigator>
   )
 }
